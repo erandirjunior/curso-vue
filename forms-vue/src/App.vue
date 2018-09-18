392934190ba218312202b2910702d385231174d6
Erandir <aefs12junior@gmail.com>
2018-09-18 18:00:09 -0300

    Criando projeto para trabalhar com formulário

---
+++ forms-vue/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+        <forms-component></forms-component>
+    </div>
+</template>
+
+<script>
+import FormsComponent from './components/FormsComponent'
+
+export default {
+    name: 'app',
+    components: {
+        FormsComponent
+    }
+}
+</script>
+
+<style>
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
---
+++ forms-vue/src/components/FormsComponent.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <form>
+            <input type="text" placeholder="Nome">
+            <hr>
+
+            <input type="email" placeholder="E-mail">
+            <hr>
+
+            <input type="radio" name="sex" value="M"> Masculino
+            <input type="radio" name="sex" value="F"> Feminino
+            <hr>
+
+            <select>
+                <option value="">Selecione o Estado</option>
+                <option value="CE">Ceará</option>
+                <option value="MA">Maranhão</option>
+                <option value="RN">Rio Grande do Norte</option>
+            </select>
+            <hr>
+
+            <label for="agree">Concordo com os termos de uso</label>
+            <input type="checkbox" id="agree">
+            <hr>
+
+            <textarea cols="30" rows="10"></textarea>
+            <hr>
+
+            <button type="submit">Enviar Agora</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
