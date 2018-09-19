9444c0d98100444dfa76e671eeddba1cd6a7168a
Erandir <aefs12junior@gmail.com>
2018-09-19 20:22:17 -0300

    Preparando projeto de lista de tarefas

---
+++ tasks/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="app">
+        <list-tasks></list-tasks>
+    </div>
+</template>
+
+<script>
+    import ListTasks from './components/Tasks/ListTasks'
+
+    export default {
+        name: 'app',
+        components: {
+            ListTasks
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ tasks/src/components/Tasks/ListTasks.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>{{ title }}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ListTasks",
+        data () {
+            return {
+                title: 'Lista de Tarefas'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
