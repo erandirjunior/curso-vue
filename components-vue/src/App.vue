cf50556e32a699b3fb837fcb50ec150b4fc68510
Erandir <aefs12junior@gmail.com>
2018-09-19 19:45:22 -0300

    Definido configurações em propriedades recebidas

--- components-vue/src/App.vue
+++ components-vue/src/App.vue
@@ -1,16 +1,19 @@
 <template>
     <div id="app">
-      <tasks-component :items="['Lavar', 'Passar', 'Cozinhar']" :title="'Lista de Tarefas'"></tasks-component>
+        <tasks-component :items="['Lavar', 'Passar', 'Cozinhar']" :title="'Lista de Tarefas'"></tasks-component>
+        <list-items-component :title="'Listagem de itens'" :items="[1, 2, 3, 4]"></list-items-component>
     </div>
 </template>
 
 <script>
 import TasksComponent from './components/TasksComponent'
+import ListItemsComponent from './components/ListItemsComponent'
 
 export default {
     name: 'app',
     components: {
-      TasksComponent
+        TasksComponent,
+        ListItemsComponent
     }
 }
 </script>
---
+++ components-vue/src/components/ListItemsComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h2>{{ title }}</h2>
+        <ul>
+            <li v-for="(item, index) in items" :key="index">{{ item }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        /*definindo configurações para uma propriedade recebida*/
+        props: {
+            title: {
+                required: true,
+                type: String,
+                default: ''
+            },
+            items: {
+                required: true,
+                type: Array,
+                default: []
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
