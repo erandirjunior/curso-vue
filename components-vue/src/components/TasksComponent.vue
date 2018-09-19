55deae7b66722111b2c74f4928116d2ae44d25f5
Erandir <aefs12junior@gmail.com>
2018-09-19 19:32:34 -0300

    Passando dados entre componentes

--- components-vue/src/components/TasksComponent.vue
+++ components-vue/src/components/TasksComponent.vue
@@ -1,14 +1,18 @@
 <template>
     <div>
         <h2>{{ title }}</h2>
+        <ul>
+            <li v-for="(item, index) in items" :key="index">{{ item }}</li>
+        </ul>
     </div>
 </template>
 
 <script>
     export default {
+        /*propriedade que indica quais componentes devem ser passados para esse componente*/
+        props: ['items', 'title'],
         data () {
             return {
-                title: 'Lista de Tarefas'
             }
         }
     }
