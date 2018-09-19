8d8f9977f04f8371e79c0271a162541d06e50d91
Erandir <aefs12junior@gmail.com>
2018-09-19 19:35:12 -0300

    Passando dados entre componentes

--- components-vue/src/App.vue
+++ components-vue/src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="app">
-      <tasks-component></tasks-component>
+      <tasks-component :items="['Lavar', 'Passar', 'Cozinhar']" :title="'Lista de Tarefas'"></tasks-component>
     </div>
 </template>
 
@@ -20,7 +20,6 @@ export default {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
     color: #2c3e50;
     margin-top: 60px;
 }
