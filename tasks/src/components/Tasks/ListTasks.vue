928c0fbfbd99d0c5ee578afc4816cc4c6fd52213
Erandir <aefs12junior@gmail.com>
2018-09-23 16:17:27 -0300

    Criando tabela para listagem dos dados

--- tasks/src/components/Tasks/ListTasks.vue
+++ tasks/src/components/Tasks/ListTasks.vue
@@ -1,6 +1,26 @@
 <template>
     <div>
         <h2>{{ title }}</h2>
+
+        <table class="table table-dark">
+            <thead>
+                <tr>
+                    <td>Id</td>
+                    <td>Nome</td>
+                    <td width="180px">Ações</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(task, index) in tasks" :key="index">
+                    <td>{{ task.id }}</td>
+                    <td>{{ task.name }}</td>
+                    <td>
+                        <a href="" class="btn btn-info">Editar</a>
+                        <a href="" class="btn btn-danger">Deletar</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
@@ -9,12 +29,17 @@
         name: "ListTasks",
         data () {
             return {
-                title: 'Lista de Tarefas'
+                title: 'Lista de Tarefas',
+                tasks: [
+                    {id: 12, name: 'Teste'}
+                ]
             }
         }
     }
 </script>
 
 <style scoped>
-
+.btn {
+    margin-right: 5px;
+}
 </style>
