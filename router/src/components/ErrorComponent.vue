c4fae865c8045b7c7f023d9fa85c68e5cf3db316
Erandir <aefs12junior@gmail.com>
2018-09-25 19:12:05 -0300

    Criando componente para páginas não encontradas

---
+++ router/src/components/ErrorComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>{{ title }}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                title: 'Erro 404'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- router/src/routes.js
+++ router/src/routes.js
@@ -1,6 +1,7 @@
 import HomeComponent from './components/HomeComponent'
 import ProductsComponent from './components/ProductsComponent'
 import TasksComponent from './components/TasksComponent'
+import ErrorComponent from './components/ErrorComponent'
 
 // Definição das rotas
 export default [
@@ -8,4 +9,5 @@ export default [
     {path: '/products', component: ProductsComponent, name: 'produtos'},
     {path: '/task/:id', component: TasksComponent, name: 'tarefa', props: true},
     {path: '/redirect', redirect: '/products'},
+    {path: '*', component: ErrorComponent},
 ]
