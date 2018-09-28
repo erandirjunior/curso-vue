9348ec6445e82d691e1d49f72bb3ff43b50ca100
Erandir <aefs12junior@gmail.com>
2018-09-28 20:13:01 -0300

    Gerando perguntas dinâmicamente

--- animations-transitions/src/components/Animations/QuestionComponent.vue
+++ animations-transitions/src/components/Animations/QuestionComponent.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
         <h2 v-text="question"></h2>
+
+        <form @submit.prevent="checkResult">
+            <input type="text" placeholder="Resposta:" v-model="reply">
+            <button type="submit">Responder</button>
+        </form>
     </div>
 </template>
 
@@ -9,7 +14,29 @@
         name: "Question",
         data () {
             return {
-                question: 'Gerando Pergunta...'
+                question: 'Gerando Pergunta...',
+                result: 0,
+                reply: ''
+            }
+        },
+        created () {
+            this.generateQuestion()
+        },
+        methods: {
+            generateQuestion () {
+                let numberOne = this.generateRandomNumber(1, 100)
+                let numberTwo = this.generateRandomNumber(1, 100)
+
+                this.result = numberOne + numberTwo
+
+                this.question = `Qual é o resultado da soma entre ${numberOne} + ${numberTwo} ?`;
+            },
+            generateRandomNumber (min, max) {
+                return Math.round(Math.random() * (max - min)) + min
+            },
+            checkResult () {
+                let msg = this.result == this.reply ? 'Acertou!' : 'Errou!'
+                alert(msg)
             }
         }
     }
