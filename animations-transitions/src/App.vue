74420f972ac20a0372c149c28afdcfb694f6fbd0
Erandir <aefs12junior@gmail.com>
2018-09-28 19:51:39 -0300

    Iniciando com animação

---
+++ animations-transitions/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id="app">
+        <button @click.prevent="show = !show">Mostrar</button>
+        <transition name="teste">
+            <div v-if="show">Mostrando...</div>
+        </transition>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'app',
+        data () {
+            return {
+                msg: 'Welcome',
+                show: false
+            }
+        }
+    }
+</script>
+
+<style>
+    .teste-leave-active {
+        transition: opacity 1.5s;
+    }
+    .teste-enter-active {
+        transition: opacity 1.5s;
+    }
+
+    .teste-enter, .teste-leave-to {
+        opacity: 0;
+    }
+
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
