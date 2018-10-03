2be7e8068a867656ddc0c1f997a2f9a1b62e633b
Erandir <aefs12junior@gmail.com>
2018-10-03 19:57:03 -0300

    Exibindo imagem de carregamento

--- vue-resources/src/components/SearchCepComponent.vue
+++ vue-resources/src/components/SearchCepComponent.vue
@@ -7,6 +7,10 @@
             <button type="submit">Buscar</button>
         </form>
 
+        <div v-if="preloader">
+            <img src="../assets/preloader.gif" alt="Carregando..">
+        </div>
+
         <div v-show="address.bairro">
             <p><b>Bairro:</b> {{ address.bairro }}</p>
             <p><b>Logradouro:</b> {{ address.logradouro }}</p>
@@ -23,15 +27,19 @@
             return {
                 title: 'Buscar Cep',
                 cep: '',
-                address: {}
+                address: {},
+                preloader: false
             }
         },
         methods: {
             onSubmit () {
+                this.preloader = true
                 this.$http.get(`https://api.postmon.com.br/v1/cep/${this.cep}`).
                 then(response => {
                     this.address = response.body
-                }, error => console.log(error))
+                }, error => {
+                    console.log(error)
+                }).finally(() => this.preloader = false)
             }
         }
     }
