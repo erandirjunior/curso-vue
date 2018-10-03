e2649771557b5b420e0dc219ce91cef77f00218f
Erandir <aefs12junior@gmail.com>
2018-10-03 20:21:05 -0300

    Criação do novo projeto

---
+++ resource-api/src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(VueRouter)
+Vue.use(VueResource)
+
+Vue.config.productionTip = false
+
+new Vue({
+    render: h => h(App)
+}).$mount('#app')
