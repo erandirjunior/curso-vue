1de2e066e13e538680d2d1b3c64bb50239dafd7f
Erandir <aefs12junior@gmail.com>
2018-10-07 19:33:57 -0300

    Exibindo validações

--- resource-api/src/components/product/ProductCreateComponent.vue
+++ resource-api/src/components/product/ProductCreateComponent.vue
@@ -3,8 +3,16 @@
         <h1>{{ title }}</h1>
 
         <form @submit.prevent="save">
+            <div v-if="errorsValidation.name" class="alert alert-danger" role="alert">
+                <p v-for="(error, index) in errorsValidation.name" :key="index">{{ error }}</p>
+            </div>
+
             <div class="form-group">
                 <input type="text" class="form-control" placeholder="Nome" v-model="product.name">
+            </div>
+
+            <div v-if="errorsValidation.description" class="alert alert-danger" role="alert">
+                <p v-for="(error, index) in errorsValidation.description" :key="index">{{ error }}</p>
             </div>
 
             <div class="form-group">
@@ -27,7 +35,8 @@
                 product: {
                     name: '',
                     description: ''
-                }
+                },
+                errorsValidation: ''
             }
         },
         methods: {
@@ -37,6 +46,9 @@
                         //console.log(response)
                         this.$router.push('/product')
                     }, error => {
+                        if (error.status == 422) {
+                            this.errorsValidation = error.body.errors
+                        }
                         console.log(error)
                     })
                     .finally(() => console.log('Finalizou!!!'))
