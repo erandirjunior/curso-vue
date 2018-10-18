042f3f488e036feac4aca896ee040f43d3627672
Erandir <aefs12junior@gmail.com>
2018-10-18 19:49:21 -0300

    Fazendo requisição com axios

--- curso-axios/src/components/SearchCepComponent.vue
+++ curso-axios/src/components/SearchCepComponent.vue
@@ -6,21 +6,38 @@
             <input type="text" placeholder="CEP" v-model="cep">
             <button type="submit">Buscar</button>
         </form>
+
+        <div v-show="address.cep">
+            <p>CEP: {{ address.cep }}</p>
+            <p>Logradouro: {{ address.logradouro }}</p>
+            <p>bairro: {{ address.bairro }}</p>
+            <p>localidade: {{ address.localidade }}</p>
+            <p>UF: {{ address.uf }}</p>
+        </div>
     </div>
 </template>
 
 <script>
+    import axios from 'axios'
+
     export default {
         name: "SearchCepComponent",
         data () {
             return {
                 title: 'Buscar CEP',
-                cep: ''
+                cep: '',
+                address: {}
             }
         },
         methods: {
             onSubmit () {
-                alert(this.cep)
+                axios.get(`http://viacep.com.br/ws/${this.cep}/json/`)
+                    .then(response => {
+                        this.address = response.data
+                        console.log(response)
+                    })
+                    .catch(error => console.log(error))
+                    .finally(() => console.log('Finalizado'))
             }
         }
     }
