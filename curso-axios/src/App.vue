e213fc964211f3b4e0326fa2fa6f00f1db24e927
Erandir <aefs12junior@gmail.com>
2018-10-18 19:35:38 -0300

    Criando componente de busca

--- curso-axios/src/App.vue
+++ curso-axios/src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <search-cep-component></search-cep-component>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import SearchCepComponent from './components/SearchCepComponent'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            SearchCepComponent
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ curso-axios/src/components/SearchCepComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1 v-text="title"></h1>
+
+        <form @submit.prevent="onSubmit">
+            <input type="text" placeholder="CEP" v-model="cep">
+            <button type="submit">Buscar</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SearchCepComponent",
+        data () {
+            return {
+                title: 'Buscar CEP',
+                cep: ''
+            }
+        },
+        methods: {
+            onSubmit () {
+                alert(this.cep)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
